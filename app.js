85ddca3335fd706d15bff4096814f9a6675379ca
Mehrab-developer <110405427+Mehrab-developer@users.noreply.github.com>
2024-06-20 00:32:15 +0500

    correct folder structure

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 const port = process.env.PORT || 3000; 
 
 
+
+
 // Start the server
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
