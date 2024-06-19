b8754ea44c669f59fc63c421e4bf97d0fa3fd55b
Mehrab-developer <110405427+Mehrab-developer@users.noreply.github.com>
2024-06-20 00:17:47 +0500

    RBAC structure added

---
+++ app.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000; 
+
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
