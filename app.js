3394b337604bfedfe2a47216f8956372072e360d
Mehrab-developer <110405427+Mehrab-developer@users.noreply.github.com>
2024-06-20 16:44:22 +0500

    MongoDB Connection

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000; 
+const connectDB = require('./config/db');
+
+connectDB();
+
 
 
 
